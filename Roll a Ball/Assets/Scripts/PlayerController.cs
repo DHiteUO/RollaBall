8028f652cc7d3171047dc1e906aa7e7897268342
Dylan Hite <dhite@cs.uoregon.edu>
2020-04-03 11:46:49 -0700

    adding assets

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpSpeed;
+    int jumpcount = 0;
+    public Text countText;
+    public Text winText;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpcount < 2)
+        {
+            rb.AddForce(Vector3.up * jumpSpeed);
+            jumpcount += 1;
+        }
+    }
+    void resetJump()
+    {
+        jumpcount = 0;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            resetJump();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
